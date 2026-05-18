e141b843a6a6a7f5dfa5e1a55f099ef503c0075f
Elmir Məmmədzadə <elmirmmmdzad0@gmail.com>
2026-05-18 13:29:27 +0400

    Make registration email async

--- backend/src/main/java/com/tripgen/api/EmailService.java
+++ backend/src/main/java/com/tripgen/api/EmailService.java
@@ -2,6 +2,7 @@ package com.tripgen.api;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,19 +14,26 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    @Async
     public void sendVerificationEmail(String toEmail, String username, String verificationLink) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("TripGen email verification");
-        message.setText("""
-                Salam %s,
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject("TripGen email verification");
+            message.setText("""
+                    Salam %s,
 
-                TripGen hesabini aktiv etmek ucun asagidaki linke daxil olun:
-                %s
+                    TripGen hesabini aktiv etmek ucun asagidaki linke daxil olun:
+                    %s
 
-                Eger bu qeydiyyati siz etmemisinizse, bu emaili nezere almayin.
-                """.formatted(username, verificationLink));
+                    Eger bu qeydiyyati siz etmemisinizse, bu emaili nezere almayin.
+                    """.formatted(username, verificationLink));
 
-        mailSender.send(message);
+            mailSender.send(message);
+            System.out.println("[AUTH][EMAIL][ASYNC_SUCCESS] Verification email sent to " + toEmail);
+        } catch (Exception e) {
+            System.out.println("[AUTH][EMAIL][ASYNC_ERROR] Email could not be sent to " + toEmail + ": " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
